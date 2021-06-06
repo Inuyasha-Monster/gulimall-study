7d59aabce5974d4abbce62a66e9f9e239de70b29
djl394922860 <394922860@qq.com>
2021-06-06 18:54:35 +0800

    支持配置动态刷新

--- gulimall-third-party/src/main/java/com/atguigu/gulimall/thirdparty/controller/OssController.java
+++ gulimall-third-party/src/main/java/com/atguigu/gulimall/thirdparty/controller/OssController.java
@@ -7,6 +7,7 @@ import com.aliyun.oss.model.PolicyConditions;
 import com.atguigu.common.utils.R;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+@RefreshScope
 @RestController
 public class OssController {
 
