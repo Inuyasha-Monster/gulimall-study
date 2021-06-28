c7f254465d9a967288f9d635906354de5d8d37d0
djl394922860 <394922860@qq.com>
2021-06-28 22:38:03 +0800

    初步实现单点登录的基本原理,等待测试通过

---
+++ gulimall-test-sso-server/src/main/java/com/atguigu/gilimall/ssoserver/GilimallTestSsoServerApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gilimall.ssoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GilimallTestSsoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GilimallTestSsoServerApplication.class, args);
+	}
+
+}
