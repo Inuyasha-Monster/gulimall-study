af200faab76b5e96ec1336cfc30a0fe100550add
djl394922860 <394922860@qq.com>
2021-07-13 21:21:47 +0800

    完成后台秒杀活动CURD以及秒杀活动关联商品的功能

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/service/impl/SeckillSkuRelationServiceImpl.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/service/impl/SeckillSkuRelationServiceImpl.java
@@ -1,7 +1,9 @@
 package com.atguigu.gulimall.coupon.service.impl;
 
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -11,6 +13,7 @@ import com.atguigu.common.utils.Query;
 import com.atguigu.gulimall.coupon.dao.SeckillSkuRelationDao;
 import com.atguigu.gulimall.coupon.entity.SeckillSkuRelationEntity;
 import com.atguigu.gulimall.coupon.service.SeckillSkuRelationService;
+import org.springframework.util.StringUtils;
 
 
 @Service("seckillSkuRelationService")
@@ -18,9 +21,24 @@ public class SeckillSkuRelationServiceImpl extends ServiceImpl<SeckillSkuRelatio
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+
+        QueryWrapper<SeckillSkuRelationEntity> queryWrapper = new QueryWrapper<SeckillSkuRelationEntity>();
+
+        //1、获取key
+        String key = (String) params.get("key");
+
+        String promotionSessionId = (String) params.get("promotionSessionId");
+
+        if (!StringUtils.isEmpty(key)) {
+            queryWrapper.eq("id", key);
+        }
+
+        if (!StringUtils.isEmpty(promotionSessionId)) {
+            queryWrapper.eq("promotion_session_id", promotionSessionId);
+        }
         IPage<SeckillSkuRelationEntity> page = this.page(
                 new Query<SeckillSkuRelationEntity>().getPage(params),
-                new QueryWrapper<SeckillSkuRelationEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
