c6116befb7ee061ee2d0902aa0b47cdf3871ae4e
djl394922860 <394922860@qq.com>
2021-07-13 23:13:52 +0800

    实现秒杀服务自动上架秒杀商品以及商城首页展示秒杀商品

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/controller/SeckillSessionController.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/controller/SeckillSessionController.java
@@ -1,15 +1,12 @@
 package com.atguigu.gulimall.coupon.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 //import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.atguigu.gulimall.coupon.entity.SeckillSessionEntity;
 import com.atguigu.gulimall.coupon.service.SeckillSessionService;
@@ -30,6 +27,18 @@ import com.atguigu.common.utils.R;
 public class SeckillSessionController {
     @Autowired
     private SeckillSessionService seckillSessionService;
+
+    /**
+     * 查询最近三天需要参加秒杀商品的信息
+     * @return
+     */
+    @GetMapping(value = "/Lates3DaySession")
+    public R getLates3DaySession() {
+
+        List<SeckillSessionEntity> seckillSessionEntities = seckillSessionService.getLates3DaySession();
+
+        return R.ok().setData(seckillSessionEntities);
+    }
 
     /**
      * 列表
---
+++ gulimall-seckill/src/main/java/com/atguigu/gulimall/seckill/config/ScheduledConfig.java
@@ -0,0 +1,16 @@
+package com.atguigu.gulimall.seckill.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author djl
+ * @create 2021/7/13 21:44
+ */
+
+@EnableAsync
+@EnableScheduling
+@Configuration
+public class ScheduledConfig {
+}
